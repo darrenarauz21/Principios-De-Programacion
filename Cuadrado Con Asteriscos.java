ead0ea2ca1575f65e531679708935c4b74b43dc4
darrenarauz21 <88292002+darrenarauz21@users.noreply.github.com>
2021-09-22 18:04:39 -0500

    Create Cuadrado Con Asteriscos.java
    imprimir cuadrado con asteriscos

---
+++ Cuadrado Con Asteriscos.java
@@ -0,0 +1,19 @@
+//imprimir cuadrado con asteriscos
+public class Cuadrado {
+	
+	public static void main (String[] args)
+	{
+		int i;
+		for(i=0;i<5;i++)
+		{
+
+		        for (int j=0; j <5; j++)
+		        	{
+		            	System.out.print(" *");
+		            }
+		            
+		        System.out.println();
+		}
+		
+	}
+}
