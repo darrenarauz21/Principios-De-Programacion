1733571421aab16d1c7c6df4bd2d68a59447719a
darrenarauz21 <88292002+darrenarauz21@users.noreply.github.com>
2021-09-22 17:34:19 -0500

    Create Suma De Fraciones.java

---
+++ Suma De Fraciones.java
@@ -0,0 +1,16 @@
+public class SumaDeFraciones {
+		public static void main (String[] args) 
+			{
+			int n=1,d=2,t=50; //n es el numerador y d el denominador, t indica el tamaño de la suma
+			float div=0,total=0;
+			System.out.print(" "+n);
+			while (d<=t) 
+			{
+				System.out.println(" + "+n+"/"+d);
+				div=div+(float)n/d;
+				d++;
+			}
+			total=div+n;
+			System.out.println("Solución: "+total);
+		}
+}
